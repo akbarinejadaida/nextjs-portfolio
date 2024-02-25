b7071ba7cc86b4127bc68e3a115cc0e746a311d5
aida_akj <aida6488@gmail.com>
2024-02-25 17:30:18 +0330

    first commit

---
+++ components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <section>
+      <h1 className='text-white'>
+        Hello, I am Aida
+      </h1>
+
+      <p>
+
+      </p>
+    </section>
+  )
+}
